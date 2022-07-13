7db2fb12844fb8774456746d6a9361933f970f82
Jeremy Huynh <jeahuynh18@gmail.com>
2022-07-13 14:24:17 -0700

    added helmtet properties to Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>Home</h1>
-                <p>Lookin cute.</p>
-            </div>
+            <>
+                <div>
+                    <Helmet>
+                        <title>Navbar-Router-Test | Home</title>
+                    </Helmet>
+                </div>
+                <div>
+                    <h1>Home</h1>
+                    <p>Lookin cute.</p>
+                </div>
+            </>
         )
     }
 };
